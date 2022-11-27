feff274dab816cfa7e343eb60bd5affef9c4defc
nullchinchilla <nullchinchilla@pm.me>
2022-11-27 17:56:51 -0600

    small fixes

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,13 +1,21 @@
-use std::{convert::Infallible, fmt::Display, str::FromStr};
+use std::{convert::Infallible, fmt::Display, ops::Add, str::FromStr};
 
 use async_trait::async_trait;
 use nanorpc::nanorpc_derive;
 use serde::{Deserialize, Serialize};
 use smol_str::SmolStr;
 
-/// This represents a backhaul-specific melnet address. It's essentially an immutable string.
+/// This represents a backhaul-specific melnet address. It's essentially an immutable string, and is not guaranteed to be valid in any way.
+///
+/// [Address] implements the `From<T>` where `T: AsRef<str>`, so any "stringy" type can be converted into an [Address].
 #[derive(Clone, Debug, PartialEq, PartialOrd, Ord, Eq, Hash, Serialize, Deserialize)]
 pub struct Address(SmolStr);
+
+impl<T: AsRef<str>> From<T> for Address {
+    fn from(t: T) -> Self {
+        Self(t.into())
+    }
+}
 
 impl Display for Address {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -19,18 +27,6 @@ impl FromStr for Address {
     type Err = Infallible;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Ok(Self(s.into()))
-    }
-}
-
-impl From<&str> for Address {
-    fn from(s: &str) -> Self {
-        Self(s.into())
-    }
-}
-
-impl From<String> for Address {
-    fn from(s: String) -> Self {
-        Self(s.into())
     }
 }
 
